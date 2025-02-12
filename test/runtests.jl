eba049a357a7dac8ec70e9a4e4ec03f7fba46cf6
Caleb Derrickson <Calebderrickson@gmail.com>
2025-02-12 17:26:52 -0600

    Fixed tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,22 @@ using Ipopt, NLPModelsJuMP
 using MadNLP, MadNLPGPU
 using SparseArrays
 using LinearAlgebra
+using CUDA
 
 using .VecchiaMLE
 
 include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 include("test_cpu_compatible_with_jump.jl")
-include("test_gpu_compatible_with_jump.jl")
 include("test_cpu_compatible_with_row.jl")
-include("test_gpu_compatible_with_row.jl")
-include("test_cpu_compatible_with_gpu.jl")
-include("test_gpu_diagnostics.jl")
 include("test_cpu_diagnostics.jl")
+
+if CUDA.has_cuda()
+    include("test_gpu_compatible_with_jump.jl")
+    include("test_gpu_compatible_with_row.jl")
+    include("test_gpu_diagnostics.jl")
+    include("test_cpu_compatible_with_gpu.jl")
+end
+
+
 include("test_model_inputs.jl")
