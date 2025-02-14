d930a1e2ffad449de7ca127b1fb44c06c0f1df2c
Caleb Derrickson <68564671+CalebDerrickson@users.noreply.github.com>
2025-02-14 10:09:05 -0600

    Update VecchiaMLE_utils.jl
    Changed randn to rand in generate_Samples

--- src/VecchiaMLE_utils.jl
+++ src/VecchiaMLE_utils.jl
@@ -16,10 +16,10 @@ end
 
 function generate_Samples(MatCov::AbstractMatrix, n::Integer, Number_of_Samples::Integer)::Matrix{Float64}
     if CUDA.has_cuda()
-        V = CUDA.rand(Number_of_Samples, n^2)
+        V = CUDA.rand(Float64, Number_of_Samples, n^2)
         S = CuArray{Float64}(MatCov)
     else 
-        V = randn(Number_of_Samples, n^2)
+        V = rand(Number_of_Samples, n^2)
         S = Matrix{Float64}(MatCov)
     end
     LinearAlgebra.LAPACK.potrf!('L', S)
