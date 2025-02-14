3f474228f5ee878a11f543c52f3a3388bc824f8c
Caleb Derrickson <68564671+CalebDerrickson@users.noreply.github.com>
2025-02-14 10:15:49 -0600

    Update runtests.jl
    Removed stupid tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,12 +12,10 @@ using .VecchiaMLE
 include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 include("test_cpu_compatible_with_jump.jl")
-include("test_cpu_compatible_with_row.jl")
 include("test_cpu_diagnostics.jl")
 
 if CUDA.has_cuda()
     include("test_gpu_compatible_with_jump.jl")
-    include("test_gpu_compatible_with_row.jl")
     include("test_gpu_diagnostics.jl")
     include("test_cpu_compatible_with_gpu.jl")
 end
