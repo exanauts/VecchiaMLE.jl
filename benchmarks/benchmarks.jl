014ec402bf2667e996cc83d88185aedc358d2aed
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-02-20 01:33:25 -0600

    Update the benchmarks (#10)
    * Update the benchmarks
    
    * Update benchmarks.jl
    
    * Update benchmarks

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -1,12 +1,14 @@
-using VecchiaMLE
+using VecchiaMLE, CUDA, DelimitedFiles
 
-# Things for model
-ns = 10:10:200
-ns = 10:10:10
+# Parameters
+# ns = 10:10:200
+ns = 10:5:100
 k = 10
 Number_of_Samples = 100
 params = [5.0, 0.2, 2.25, 0.25]
-timings = zeros(2, length(ns))
+timings_linalg = zeros(2, ns |> length)
+timings_model = zeros(2, ns |> length)
+timings_solve = zeros(2, ns |> length)
 
 for (i, n) in enumerate(ns)
     # Generate samples
@@ -18,9 +20,29 @@ for (i, n) in enumerate(ns)
 
     # CPU
     input.mode = 1
-    d_cpu, L_cpu = VecchiaMLE_Run(input)
+    diagnostics_cpu, L_cpu = VecchiaMLE_Run(input)
+    timings_model[1, i] = diagnostics_cpu.create_model_time
+    timings_solve[1, i] = diagnostics_cpu.solve_model_time
+    timings_linalg[1, i] = diagnostics_cpu.LinAlg_solve_time
 
     # GPU
-    input.mode = 2
-    d_gpu, L_gpu = VecchiaMLE_Run(input)
+    if CUDA.has_cuda()
+        input.mode = 2
+        diagnostics_gpu, L_gpu = VecchiaMLE_Run(input)
+        timings_model[2, i] = diagnostics_gpu.create_model_time
+        timings_solve[2, i] = diagnostics_gpu.solve_model_time
+        timings_linalg[2, i] = diagnostics_gpu.LinAlg_solve_time
+    end
+end
+
+open("timings_linalg.txt", "w") do io
+    writedlm(io, timings_linalg)
+end
+
+open("timings_model.txt", "w") do io
+    writedlm(io, timings_model)
+end
+
+open("timings_solve.txt", "w") do io
+    writedlm(io, timings_solve)
 end
