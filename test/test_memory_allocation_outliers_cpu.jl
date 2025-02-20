4e063366824230188e055446b917162c3ff40af8
Caleb Derrickson <calebderrickson@gmail.com>
2025-02-20 14:03:42 -0600

    fixed mistakes

--- test/test_memory_allocation_outliers_cpu.jl
+++ test/test_memory_allocation_outliers_cpu.jl
@@ -8,12 +8,11 @@
     samples = VecchiaMLE.generate_Samples(MatCov, n, Number_of_Samples)
     xyGrid = VecchiaMLE.generate_xyGrid(n)
 
-    # Get result from VecchiaMLE CPU
-    
     model_cpu = VecchiaMLE.VecchiaModelCPU(samples, k, xyGrid)
+    mems = test_allocs_nlpmodels(model_cpu)
 
     # Should only expect to see two values, one where we need two vectors and the other where we need one
-    mem_arr = sort(unique([x for x in values(model_cpu) if (!isnan(x) && x > 0.0)]))
+    mem_arr = sort(unique([mems[i] for i in mems if (!isnan(mems[i]) && mems[i] > 0.0)]))
 
     @test (length(mem_arr) == 2)
     @test (mem_arr[2] / mem_arr[1] ≈ 2.0)
--- test/test_memory_allocation_outliers_gpu.jl
+++ test/test_memory_allocation_outliers_gpu.jl
@@ -7,13 +7,12 @@
     MatCov = VecchiaMLE.generate_MatCov(n, params)
     samples = VecchiaMLE.generate_Samples(MatCov, n, Number_of_Samples)
     xyGrid = VecchiaMLE.generate_xyGrid(n)
-
-    # Get result from VecchiaMLE CPU
     
-    model_cpu = VecchiaMLE.VecchiaModelGPU(samples, k, xyGrid)
+    model_gpu = VecchiaMLE.VecchiaModelGPU(samples, k, xyGrid)
+    mems = test_allocs_nlpmodels(model_gpu)
 
     # Should only expect to see two values, one where we need two vectors and the other where we need one
-    mem_arr = sort(unique([x for x in values(model_cpu) if (!isnan(x) && x > 0.0)]))
+    mem_arr = sort(unique([mems[i] for i in mems if (!isnan(mems[i]) && mems[i] > 0.0)]))
 
     @test (length(mem_arr) == 2)
     @test (mem_arr[2] / mem_arr[1] ≈ 2.0)
