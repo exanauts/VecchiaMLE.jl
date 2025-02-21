0cf09bf5b50c0aafd107a013b31e2ff68be02efa
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-02-20 23:26:49 -0600

    wip...

--- src/VecchiaMLE_kernels.jl
+++ src/VecchiaMLE_kernels.jl
@@ -33,7 +33,8 @@
 
 #     # Launch the kernel
 #     backend = KA.get_backend(y)
-#     vecchia_mul_kernel!(backend)(y, hess_obj_vals, x, CuVector(m), n, colptrL, offsets, ndrange=n)
+#     kernel = vecchia_mul_kernel!(backend)
+#     kernel(y, hess_obj_vals, x, CuVector(m), n, colptrL, offsets, ndrange=n)
 #     KA.synchronize(backend)
 #     return y
 # end
@@ -65,27 +66,27 @@ end
 @kernel function vecchia_build_B_kernel!(hess_obj_vals, @Const(samples), @Const(rowsL), @Const(colptrL),
                                          @Const(m), @Const(n), @Const(r))
     index = @index(Global)
+    pos = colptrL[index]
+    mj = m[index]
 
-    if index <= n
-        pos = colptrL[index]
-        mj = m[index]
-
-        k = 0
-        for s in 1:mj
-            for t in s:mj
-                acc = 0.0
-                for i = 1:r
-                    acc += samples[i, rowsL[pos+t-1]] * samples[i, rowsL[pos+s-1]]
-                end
-                hess_obj_vals[pos+k] = acc
-                k = k + 1
+    k = 0
+    for s in 1:mj
+        for t in s:mj
+            acc = 0.0
+            for i = 1:r
+                acc += samples[i, rowsL[pos+t-1]] * samples[i, rowsL[pos+s-1]]
             end
+            hess_obj_vals[pos+k] = acc
+            k = k + 1
         end
     end
     nothing
 end
 
 function vecchia_build_B!(B::Vector{Matrix{T}}, samples::CuMatrix{T}, rowsL::Vector{Int}, colptrL::Vector{Int}, hess_obj_vals::CuVector{T}, n::Int, m::Vector{Int}) where T <: AbstractFloat
+    hess_obj_vals2 = copy(hess_obj_vals)
+
+    pos = 0
     for j in 1:n
         for s in 1:m[j]
             for t in 1:m[j]
@@ -93,13 +94,26 @@ function vecchia_build_B!(B::Vector{Matrix{T}}, samples::CuMatrix{T}, rowsL::Vec
                 vs = view(samples, :, rowsL[colptrL[j] + s - 1])
                 B[j][t, s] = dot(vt, vs)
             end
+
+            # Lower triangular part of the block Bⱼ
+            if s ≤ t
+                pos = pos + 1
+                hess_obj_vals2[pos] = B[j][t, s]
+            end
         end
     end
 
     # Launch the kernel
     backend = KA.get_backend(samples)
     r = size(samples, 1)
-    vecchia_build_B_kernel!(backend)(hess_obj_vals, samples, CuVector(rowsL), CuVector(colptrL), CuVector(m), n, r, ndrange=n)
+    kernel = vecchia_build_B_kernel!(backend)
+    kernel(hess_obj_vals, samples, CuVector(rowsL), CuVector(colptrL), CuVector(m), n, r, ndrange=n)
+    println("BOB!")
+    bob = hess_obj_vals2 - hess_obj_vals
+    println(norm(bob))
+    for (i, v) in enumerate(bob)
+        println("i: $i , val: $val")
+    end
     KA.synchronize(backend)
     return nothing
 end
