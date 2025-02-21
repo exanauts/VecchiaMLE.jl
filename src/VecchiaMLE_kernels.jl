2875a4ff9b6d1963f9f159cf7c02940fd7b2ce4c
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-02-20 22:47:10 -0600

    wip...

--- src/VecchiaMLE_kernels.jl
+++ src/VecchiaMLE_kernels.jl
@@ -1,40 +1,52 @@
-@kernel inbounds=true function vecchia_mul_kernel!(y, @Const(hess_obj_vals), @Const(x), @Const(m), @Const(n), @Const(colptrL), @Const(offsets))
-    index = @index(Global)
+# @kernel inbounds=true function vecchia_mul_kernel!(y, @Const(hess_obj_vals), @Const(x), @Const(m), @Const(n), @Const(colptrL), @Const(offsets))
+#     index = @index(Global)
 
-    pos = colptrL[index]
-    offset = offsets[index]
-    mj = m[index]
+#     pos = colptrL[index]
+#     offset = offsets[index]
+#     mj = m[index]
 
-    # Perform the matrix-vector multiplication for the current symmetric block
-    for j in 1:mj
-        idx1 = j * (j-1) ÷ 2
-        for i in j:mj
-            idx2 = idx1 + (i - j + 1)
-            val = hess_obj_vals[pos-1+idx2]
+#     # Perform the matrix-vector multiplication for the current symmetric block
+#     for j in 1:mj
+#         idx1 = j * (j-1) ÷ 2
+#         for i in j:mj
+#             idx2 = idx1 + (i - j + 1)
+#             val = hess_obj_vals[pos-1+idx2]
 
-            # Diagonal element contributes only once
-            if i == j
-                y[offset+i] += val * x[offset+j]
-            else
-                y[offset+i] += val * x[offset+j]
-                y[offset+j] += val * x[offset+i]  # due to symmetry A[i,j] = A[j,i]
-            end
-        end
-    end
-    nothing
-end
+#             # Diagonal element contributes only once
+#             if i == j
+#                 y[offset+i] += val * x[offset+j]
+#             else
+#                 y[offset+i] += val * x[offset+j]
+#                 y[offset+j] += val * x[offset+i]  # due to symmetry A[i,j] = A[j,i]
+#             end
+#         end
+#     end
+#     nothing
+# end
+
+# function vecchia_mul!(y::CuVector{T}, B::Vector{<:CuMatrix{T}}, hess_obj_vals::CuVector{T}, x::CuVector{T}, n::Int, m::Vector{Int}, colptrL::CuVector{Int}) where T <: AbstractFloat
+#     # Reset the vector y
+#     fill!(y, zero(T))
+
+#     # Precompute offsets for all blocks <-- should be in VecchiaCache
+#     offsets = cumsum([0; m[1:end-1]]) |> CuVector{Int}
+
+#     # Launch the kernel
+#     backend = KA.get_backend(y)
+#     vecchia_mul_kernel!(backend)(y, hess_obj_vals, x, CuVector(m), n, colptrL, offsets, ndrange=n)
+#     KA.synchronize(backend)
+#     return y
+# end
 
 function vecchia_mul!(y::CuVector{T}, B::Vector{<:CuMatrix{T}}, hess_obj_vals::CuVector{T}, x::CuVector{T}, n::Int, m::Vector{Int}, colptrL::CuVector{Int}) where T <: AbstractFloat
-    # Reset the vector y
-    fill!(y, zero(T))
-
-    # Precompute offsets for all blocks <-- should be in VecchiaCache
-    offsets = cumsum([0; m[1:end-1]]) |> CuVector{Int}
-
-    # Launch the kernel
-    backend = KA.get_backend(y)
-    vecchia_mul_kernel!(backend)(y, hess_obj_vals, x, CuVector(m), n, colptrL, offsets, ndrange=n)
-    KA.synchronize(backend)
+    pos = 0
+    for j = 1:n
+        Bj = B[j]
+        yj = view(y, pos+1:pos+m[j])
+        xj = view(x, pos+1:pos+m[j])
+        mul!(yj, Bj, xj)
+        pos = pos + m[j]
+    end
     return y
 end
 
@@ -51,18 +63,16 @@ function vecchia_mul!(y::Vector{T}, B::Vector{Matrix{T}}, hess_obj_vals::Vector{
 end
 
 @kernel function vecchia_build_B_kernel!(hess_obj_vals, @Const(samples), @Const(rowsL), @Const(colptrL),
-                                         @Const(m), @Const(n), @Const(r), @Const(offsets))
+                                         @Const(m), @Const(n), @Const(r))
     index = @index(Global)
 
     pos = colptrL[index]
-    offset = offsets[index]
     mj = m[index]
 
-    T = eltype(samples)
     k = 0
     for t in 1:mj
         for s in t:mj
-            acc = zero(T)
+            acc = 0.0
             for i = 1:r
                 acc += samples[i, rowsL[pos + t - 1]] * samples[i, rowsL[pos + s - 1]]
             end
@@ -74,9 +84,6 @@ end
 end
 
 function vecchia_build_B!(B::Vector{Matrix{T}}, samples::CuMatrix{T}, rowsL::Vector{Int}, colptrL::Vector{Int}, hess_obj_vals::CuVector{T}, n::Int, m::Vector{Int}) where T <: AbstractFloat
-    # Precompute offsets for all blocks <-- should be in VecchiaCache
-    offsets = cumsum([0; m[1:end-1]]) |> CuVector{Int}
-
     for j in 1:n
         for s in 1:m[j]
             for t in 1:m[j]
@@ -90,7 +97,7 @@ function vecchia_build_B!(B::Vector{Matrix{T}}, samples::CuMatrix{T}, rowsL::Vec
     # Launch the kernel
     backend = KA.get_backend(samples)
     r = size(samples, 1)
-    vecchia_build_B_kernel!(backend)(hess_obj_vals, samples, CuVector(rowsL), CuVector(colptrL), CuVector(m), n, r, offsets, ndrange=n)
+    vecchia_build_B_kernel!(backend)(hess_obj_vals, samples, CuVector(rowsL), CuVector(colptrL), CuVector(m), n, r, ndrange=n)
     KA.synchronize(backend)
     return nothing
 end
