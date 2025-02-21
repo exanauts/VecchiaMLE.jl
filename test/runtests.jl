d574d68f74ab92f3207bb0d5090e7bb2da51890e
Caleb Derrickson <68564671+CalebDerrickson@users.noreply.github.com>
2025-02-21 17:02:26 -0600

    Add memory tests (#29)
    * added memory tests for cpu and gpu. maybe too strict.
    
    * fixed mistakes
    
    * reverted mistake fix
    
    * added get_vecchia_model to docs. It's always the small things I swear
    
    * wrong branch, oops
    
    * Updating tests
    
    * Update Project.toml
    
    Fix error?
    
    * Update test_memory_allocation_outliers_cpu.jl
    
    * Update test_memory_allocation_outliers_gpu.jl
    
    * Update test_memory_allocation_outliers_cpu.jl
    
    fixed typo
    
    * Update test_memory_allocation_outliers_gpu.jl
    
    fixed typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using MadNLP, MadNLPGPU
 using SparseArrays
 using LinearAlgebra
 using CUDA
+using NLPModelsTest
 
 using .VecchiaMLE
 using VecchiaMLE: CPU, GPU
@@ -14,11 +15,13 @@ include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 include("test_cpu_compatible_with_jump.jl")
 include("test_cpu_diagnostics.jl")
+include("test_memory_allocation_outliers_cpu.jl")
 
 if CUDA.has_cuda()
     include("test_gpu_compatible_with_jump.jl")
     include("test_gpu_diagnostics.jl")
     include("test_cpu_compatible_with_gpu.jl")
+    include("test_memory_allocation_outliers_gpu.jl")
 end
 
 include("test_model_inputs.jl")
