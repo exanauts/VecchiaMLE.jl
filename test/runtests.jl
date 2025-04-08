8604c87e8be98ca9b75f85af48f2667bc4c355a5
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-07 20:53:35 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,14 +16,14 @@ include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 
 include("test_cpu_compatible_with_jump.jl")
-#include("test_cpu_diagnostics.jl")
-#include("test_memory_allocation_outliers_cpu.jl")
+include("test_cpu_diagnostics.jl")
+include("test_memory_allocation_cpu.jl")
 
 if CUDA.has_cuda()
     include("test_gpu_compatible_with_jump.jl")
     include("test_gpu_diagnostics.jl")
     include("test_cpu_compatible_with_gpu.jl")
-    # include("test_memory_allocation_outliers_gpu.jl")
+    include("test_memory_allocation_gpu.jl")
 end
 
 include("test_model_inputs.jl")
