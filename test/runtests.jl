7eb22c9fdc135d584320ff860c1ad229eb7ad551
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-22 15:21:26 -0500

    Update tests for HSL

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using Test
 using JuMP
 using Ipopt, NLPModelsJuMP
 using MadNLP, MadNLPGPU
-using HSL, MadNLPHSL
 using SparseArrays
 using LinearAlgebra
 using CUDA
@@ -16,7 +15,6 @@ using VecchiaMLE: cpu, gpu
 include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 
-include("test_linear_solver.jl")
 include("test_cpu_compatible_with_jump.jl")
 include("test_cpu_diagnostics.jl")
 include("test_memory_allocation_cpu.jl")
@@ -30,3 +28,9 @@ end
 
 include("test_model_inputs.jl")
 include("test_abnormal_ptGrid.jl")
+
+using HSL, MadNLPHSL
+
+if LIBHSL_isfunctional()
+    include("test_linear_solver.jl")
+end
