3769af860f90af267c351a92c7c607a862b1c5e2
Caleb Derrickson <68564671+CalebDerrickson@users.noreply.github.com>
2025-04-22 11:24:30 -0500

    Update test_memory_allocation_cpu.jl

--- test/test_memory_allocation_cpu.jl
+++ test/test_memory_allocation_cpu.jl
@@ -6,7 +6,7 @@
     xyGrid = VecchiaMLE.generate_xyGrid(n)
     MatCov = VecchiaMLE.generate_MatCov(n, params, xyGrid)
     samples = VecchiaMLE.generate_Samples(MatCov, n, Number_of_Samples; mode=cpu)
-
+    iVecchiaMLE = VecchiaMLE.VecchiaMLEInput(n, k, samples, Number_of_Samples, 1, 5; ptGrid=xyGrid)
     model = VecchiaMLE.VecchiaModelCPU(iVecchiaMLE)
     mems = NLPModelsTest.test_allocs_nlpmodels(model)
 
