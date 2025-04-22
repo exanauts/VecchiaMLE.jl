08cf9b383e09d944865bbbfc45641f3c15b1c96d
Caleb Derrickson <68564671+CalebDerrickson@users.noreply.github.com>
2025-04-22 10:55:55 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,7 @@ using VecchiaMLE: cpu, gpu
 include("models/JumpModel.jl")
 include("models/VecchiaMLE_models.jl")
 
-include("test_linear_solver.jl")
+#include("test_linear_solver.jl")
 include("test_cpu_compatible_with_jump.jl")
 include("test_cpu_diagnostics.jl")
 include("test_memory_allocation_cpu.jl")
