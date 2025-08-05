e3a2cb80204d653c7c94ab7d2157e0a8094851f5
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-04 20:39:08 -0500

    Fix an issue with varargs

--- ext/VecchiaMLENLPModelsKnitroExt.jl
+++ ext/VecchiaMLENLPModelsKnitroExt.jl
@@ -3,8 +3,8 @@ module VecchiaMLENLPModelsKnitroExt
 import VecchiaMLE
 import NLPModelsKnitro
 
-function vecchia_solver(solver::Val{:knitro}, args..., kwargs...)
-	NLPModelsKnitro.knitro(args..., kwargs...)
+function vecchia_solver(solver::Val{:knitro}, args...; kwargs...)
+	NLPModelsKnitro.knitro(args...; kwargs...)
 end
 
 end
