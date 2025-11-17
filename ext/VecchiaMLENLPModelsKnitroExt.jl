490a25ba23b658bcbea693ce386d7c68a8f3321e
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-11-18 11:24:01 +1300

    Don't support the linear API in KNITRO.jl

--- ext/VecchiaMLENLPModelsKnitroExt.jl
+++ ext/VecchiaMLENLPModelsKnitroExt.jl
@@ -4,7 +4,7 @@ import VecchiaMLE
 import NLPModelsKnitro
 
 function VecchiaMLE.vecchia_solver(::Val{:knitro}, args...; kwargs...)
-	NLPModelsKnitro.knitro(args...; kwargs...)
+	NLPModelsKnitro.knitro(args...; linear_api=false, kwargs...)
 end
 
 end
